4a73db329bf9f149d09a6cb364eb22fc647c092f
tolak <kalot.wang@gmail.com>
2023-06-13 16:49:03 +0800

    wip: ethereum tx indexer

--- src/processor.ts
+++ src/processor.ts
@@ -1,130 +1,55 @@
-import { BatchHandlerContext, BatchProcessorItem, EvmBatchProcessor, EvmBlock } from '@subsquid/evm-processor'
-import { LogItem } from '@subsquid/evm-processor/lib/interfaces/dataSelection'
-import { Store, TypeormDatabase } from '@subsquid/typeorm-store'
-import { lookupArchive } from '@subsquid/archive-registry'
-import { In } from 'typeorm'
-import { Account, Transaction, DepositEvent } from './model'
-import { getChainConfig } from './config'
-import { WHITELIST_CONFIG } from './config/index';
+// Only for local dev
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const CHAIN_CONFIGS = getChainConfig();
+import { EvmBatchProcessor } from '@subsquid/evm-processor'
+import { TypeormDatabase } from '@subsquid/typeorm-store'
+import { Transaction } from './model/generated/transaction.model';
+import { findAccount, WORKER_ACCOUNTS } from './utils'
 
 const processor = new EvmBatchProcessor()
-    .setBlockRange(CHAIN_CONFIGS.blockRange)
-    .setDataSource(CHAIN_CONFIGS.dataSource)
-    .addLog(['0xcd38b15a419491c7c1238B0659f65c755792e257'], {
-        filter: [[
-            '0x8752a472e571a816aea92eec8dae9baf628e840f4929fbcc2d155e6233ff68a7'
-        ]],
-        data: {
-            kind: true,
-            address: true,
-            evmLog: { id: true, topics: true, data: true, name: true, address: true },
-            transaction: {
-                status: true,
-                hash: true,
-                name: true,
-            }
-        }
+    .setBlockRange({
+        from: Number(process.env.FROM_BLOCK),
+        to: Number(process.env.TO_BLOCK)
     })
-    .addTransaction([], {
-        data: {
-            transaction: {
-                nonce: true,
-                from: true,
-                to: true,
-            }
-        }
+    .setDataSource({
+        archive: String(process.env.DATA_SOURCE)
+    })
+    .setFields({
+        transaction: {
+            from: true,
+            to: true,
+            nonce: true,
+            value: true,
+            hash: true,
+            status: true,
+        },
+    })
+    // Txs sent from vitalik.eth
+    .addTransaction({
+        from: WORKER_ACCOUNTS,
     })
 
-type Item = BatchProcessorItem<typeof processor>;
+processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
+    let transactions: Transaction[] = []
 
-processor.run(new TypeormDatabase(), async ctx => {
-    let knownAccounts = await ctx.store.find(Account).then(accounts => {
-        return new Map(accounts.map(a => [a.id, a]))
-    })
-
-    console.log(knownAccounts)
-
-    let transactions: Transaction[] = [];
-    for (const block of ctx.blocks) {
-        let blockNumber = block.header.height;
-        // unix timestamp
-        let timestamp = BigInt(block.header.timestamp);
-        for (let item of block.items) {
-            if (item.kind === "transaction") {
-                let tx = item.transaction;
-                let from = tx.from!;
-                // tx like this https://etherscan.io/tx/0x37dbc681e559a4ea5b943112f291165abfbca3baf2e57653ca0ea3da33658de8
-                // doesn't have a recipient.
-                let to = tx.to!;
-                let nonce = Number(tx.nonce);
-                // note this: must agree on what case to use
-                if (WHITELIST_CONFIG.Accounts.includes(from.toLowerCase())) {
-                    console.log(item);
-                    console.log(`${from} -> ${to}: ${nonce}`);
-
-                    let account = knownAccounts.get(from);
-                    if (account === undefined) {
-                        account = new Account();
-                        account.id = from;
-                    }
-                    knownAccounts.set(account.id, account);
-
-                    // at the time of writing, 26.4.2023
-                    // subsquid hasn't fully supported EVM tx status
-                    // TODO: come back to this later
-                    let result = true;
-                    transactions.push(new Transaction({
-                        id: tx.id,
-                        account,
-                        nonce,
-                        result,
-                        blockNumber,
-                        timestamp
-                    }));
-                }
-            } else if (item.kind === 'evmLog') {
-                console.log(item)
-                if (item.evmLog && item.evmLog.data) {
-                    const { token, recipient, amount } = decodeData(item.evmLog.data);
-                    console.log(`data: ${item.evmLog.data}`);
-                    console.log(`token: ${token}`);
-                    console.log(`recipient: ${recipient}`);
-                    console.log(`amount: ${amount}`);
-                }
-
+    for (let block of ctx.blocks) {
+        for (let transaction of block.transactions) {
+            if (findAccount(transaction.from)) {
+                continue
             }
+            transactions.push(
+                new Transaction({
+                    id: transaction.id,
+                    account: transaction.from,
+                    nonce: transaction.nonce,
+                    result: transaction.status === 0,
+                    blockNumber: block.header.height,
+                    timestamp: block.header.timestamp.toString(),
+                })
+            )
         }
     }
 
-    await ctx.store.save([...knownAccounts.values()]);
-    await ctx.store.insert(transactions);
-});
-
-function decodeData(data: string): { token: string; recipient: string; amount: number } {
-    if (data.startsWith('0x')) {
-        data = data.slice(2);
-    }
-    const chunks = data.match(/.{8}/g);
-
-    if (!chunks) {
-        return {
-            token: "",
-            recipient: "",
-            amount: 0
-        }
-    }
-
-    const reversedData = chunks.map((chunk) => chunk.match(/.{2}/g)!.reverse().join('')).join('');
-
-    const token = '0x' + reversedData.slice(0, 40);
-    const recipient = '0x' + reversedData.slice(40, 80);
-    const amount = parseInt(reversedData.slice(80), 16);
-
-    return {
-        token,
-        recipient,
-        amount,
-    };
-}
+    await ctx.store.insert(transactions)
+})
